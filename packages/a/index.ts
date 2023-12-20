3710260da28a6bbd23f936546616d606e0139240
dingyanping <dingyanping@360.cn>
2023-12-20 11:43:34 +0800

    feat: 增加乘法口诀

--- packages/a/index.ts
+++ packages/a/index.ts
@@ -1,1 +1,11 @@
 export const isEven = (x: number) => x % 2 === 0;
+
+export function printMultiplicationTable() {
+  for (let i = 1; i <= 9; i++) {
+    let line = '';
+    for (let j = 1; j <= i; j++) {
+      line += `${j} * ${i} = ${j * i}  `;
+    }
+    console.log(line);
+  }
+}
